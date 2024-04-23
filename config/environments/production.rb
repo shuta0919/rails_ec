f8f3b43e468b1eb6f4260d816b74eecc43dd9980
murahiro <murahiro0919@gmail.com>
2024-04-23 12:28:29 +0900

    環境変数設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,5 +95,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # 許可するホスト名を環境変数から取得
-  config.hosts << "shuta-app-2469c2667d92.herokuapp.com"
+  config.hosts << "shielded-river-83819-eda9499a7415.herokuapp.com"
 end
