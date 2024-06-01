d1e703b543e2356568c5a2ff24726cbd2e588636
murahiro <murahiro0919@gmail.com>
2024-06-01 22:10:49 +0900

    商品一覧画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   root "products#index"
-  get 'products/index'
+  resources :products
   resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+8.times do |n|
+  product = Product.create(
+    name: "商品 #{n + 1}",
+    description: "これは商品 #{n + 1}の説明文です。",
+    price: (n + 1) * 1000
+  )
+
+  image_path = Rails.root.join('app/assets/images/test.jpg')
+  product.image.attach(io: File.open(image_path), filename: 'test.jpg')
+end
