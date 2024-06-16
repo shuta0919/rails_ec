8e3e8643e0e1f64cac4df78643fcb2a0f9f45161
murahiro <murahiro0919@gmail.com>
2024-06-16 17:14:23 +0900

    商品詳細ページ作成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,10 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def show
+    @product = Product.find(params[:id])
+
+    @products = Product.where.not(id: @product.id).order(created_at: :desc).limit(4)
+  end
 end
