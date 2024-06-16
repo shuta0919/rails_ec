aca67d5a16904e4af4f84ac64adbb8ae1e35ae1c
murahiro <murahiro0919@gmail.com>
2024-06-16 17:22:49 +0900

    rubocopにて修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root "products#index"
+  root 'products#index'
   resources :products
   resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
