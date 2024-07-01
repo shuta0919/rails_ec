a30b822f7fa1a392a8bb990a283115ccf4f7c021
murahiro <murahiro0919@gmail.com>
2024-07-01 18:06:15 +0900

    管理機能コントローラー作成、ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   root 'products#index'
   resources :products
   resources :tasks
+
+  namespace :admin do
+    resources :products
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
