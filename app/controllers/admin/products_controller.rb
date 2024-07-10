436ffaffbf025195dbce78b8b8656d5ca0a61af7
murahiro <murahiro0919@gmail.com>
2024-07-10 13:11:03 +0900

    管理者認証機能実装

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,6 @@
 class Admin::ProductsController < ApplicationController
+  before_action :authenticate
+
   def index
     @products = Product.with_attached_image
   end
@@ -18,7 +20,38 @@ class Admin::ProductsController < ApplicationController
     end
   end
 
+
+
+  def destroy
+    @product = Product.find(params[:id])
+    if @product.destroy
+      flash[:success] = "#{@product.name}が正常に削除されました"
+      redirect_to admin_products_path
+    else
+      flash[:error] = "#{@product.name}の削除に失敗しました"
+      redirect_to admin_products_path
+    end
+  end
+
+
+  def updata
+    @product = Product.find(params[:id])
+    if @product.updata(product_params)
+      flash[:success] = "#{@product.name}が更新されました"
+      redirect_to edit_admin_product_path
+    else
+      flash[:error] = "#{@product.name}の更新に失敗しました。入力内容を確認してください。"
+    end
+  end
+
   private
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password |
+      username == Rails.application.credentials.admin[:username] &&
+      password == Rails.application.credentials.admin[:password]
+    end
+  end
 
   def product_params
     params.require(:product).permit(:name, :description, :price, :image)
