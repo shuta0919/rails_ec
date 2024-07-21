9f41a7b2c85d655f887d4dbb1a152c7a122770e4
murahiro <murahiro0919@gmail.com>
2024-07-21 11:31:22 +0900

    バリデーション作成

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class Product < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :price, presence: true
+  validates :description, presence: true, length: { maximum: 1000 }
+
   has_one_attached :image
 end
