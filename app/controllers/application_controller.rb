c7c31e82ed16068cb7774cb228e75cf43adf7293
murahiro <murahiro0919@gmail.com>
2024-08-06 14:23:03 +0900

    カートのセッション機能を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  
+
+  # セッションの作成
+  def current_cart
+    current_cart = Cart.find_by(id: session[:id])
+    current_cart = Cart.create unless current_cart
+    session[:cart_id] = current_cart.id
+    return current_cart
+  end
 end
