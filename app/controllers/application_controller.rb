02d42f2bf3049e9470d8cf0cdcd9ca13fcd2ea28
murahiro <murahiro0919@gmail.com>
2024-08-07 12:43:03 +0900

    カート機能のセッション作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  
+  helper_method :current_cart
 
   # セッションの作成
   def current_cart
-    current_cart = Cart.find_by(id: session[:id])
-    current_cart = Cart.create unless current_cart
-    session[:cart_id] = current_cart.id
-    return current_cart
+    Cart.find_by(id: session[:cart_id]) || create_new_cart
+  end
+
+  def create_new_cart
+    cart = Cart.create
+    session[:cart_id] = cart.id
+    return cart
   end
 end
