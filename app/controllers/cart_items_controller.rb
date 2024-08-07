4ca33efefc394b8b8d99936a5f70238d307e2d22
murahiro <murahiro0919@gmail.com>
2024-08-07 12:46:53 +0900

    カートに商品を追加する機能を作成

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,5 +1,9 @@
 class CartItemsController < ApplicationController
   def create
+    @cart = current_cart
+    product = Product.find_by(id: params[:product_id])
+    @cart_item = @cart.add_product(product)
+    redirect_to root_path
   end
 
   def update
@@ -7,4 +11,7 @@ class CartItemsController < ApplicationController
 
   def destroy
   end
+
+
+
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,19 @@
 class Cart < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :products, through: :cart_items
+
+  def add_product(product)
+    current_item = cart_items.find_by(product_id: product.id)
+    if current_item
+      current_item.quantity += 1
+      current_item.save
+    else
+      current_item = cart_items.create(product_id: product.id, quantity: 1)
+    end
+    return current_item
+  end
+
+  def total_items
+    cart_items.sum(:quantity)
+  end
 end
