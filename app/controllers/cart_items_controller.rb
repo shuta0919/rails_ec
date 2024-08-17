36a46a5247afa8f3e8ccceac494ae10f7702b233
murahiro <murahiro0919@gmail.com>
2024-08-17 19:37:49 +0900

    カート商品削除機能を実装、フラッシュメッセージを修正

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -9,15 +9,24 @@ class CartItemsController < ApplicationController
   def update
     @cart_item = CartItem.find(params[:id])
     if @cart_item.update(cart_item_params)
-      flash[:success] = '商品数量が更新されました'
+      flash[:success] = "商品数量を更新しました"
       redirect_to cart_path
     else
-      flash[:error] = '商品数量の更新に失敗しました'
+      flash[:error] = "商品数量の更新に失敗しました"
       redirect_to cart_path
     end
   end
 
   def destroy
+    @cart_item = CartItem.find(params[:id])
+
+    if @cart_item.destroy
+      flash[:success] = "#{@cart_item.product.name}を削除しました"
+      redirect_to cart_path
+    else
+      flash[:error] = "#{@cart_item.product.name}を削除できませんでした"
+      redirect_to cart_path
+    end
   end
 
   private
