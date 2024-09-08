8c253d8e19fc9e57657eee60bbf54457f3e3ad50
murahiro <murahiro0919@gmail.com>
2024-09-08 19:26:45 +0900

    Orderモデル、OrderItemモデルにバリデーションを設定

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,16 @@
 class Order < ApplicationRecord
   has_many :order_items, dependent: :destroy
   has_many :products, through: :order_items
+
+  validates :first_name, presence: true, length: {maximum: 50}
+  validates :last_name, presence: true, length: {maximum: 50}
+  validates :email, presence: true
+  validates :address_line_1, presence: true
+  validates :country, presence: true
+  validates :state, presence: true
+  validates :zip, presence: true, format: {with: /\A\d{3}[-]?\d{4}\z/}
+  validates :cc_name, presence: true
+  validates :cc_number, presence: true, format: {with: /\A\d{14,16}\z/}
+  validates :cc_exp, presence: true, format: {with: /\A(0[1-9]|1[0-2])\/\d{2}\z/}
+  validates :cc_cvv, presence: true, format: {with: /\A\d{3,4}\z/}
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,8 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
+
+  validates :quantity, presence: true, numericality: {only_integer: true, greater_than: 0}
+  validates :price, presence: true, numericality: {greater_than: 0}
+  validates :total_price, presence: true, numericality: {greater_than: 0}
 end
