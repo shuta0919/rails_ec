036d22ed45493eddad2af2096ba6328b17e3c5cd
murahiro <murahiro0919@gmail.com>
2024-10-06 12:14:10 +0900

    Fix email sender address in ApplicationMailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: "postmaster@#{ENV['MAILGUN_DOMAIN']}"
   layout 'mailer'
 end
