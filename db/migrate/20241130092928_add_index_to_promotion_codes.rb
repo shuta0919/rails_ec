f0873cca8c42dde0941d12ce57c485ba787a911f
murahiro <murahiro0919@gmail.com>
2024-11-30 19:09:51 +0900

    promotion_codesテーブルにインデックスを追加

---
+++ db/migrate/20241130092928_add_index_to_promotion_codes.rb
@@ -0,0 +1,5 @@
+class AddIndexToPromotionCodes < ActiveRecord::Migration[7.0]
+  def change
+    add_index :promotion_codes, :code, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_30_022439) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_30_092928) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -100,6 +100,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_30_022439) do
     t.boolean "used", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["code"], name: "index_promotion_codes_on_code", unique: true
   end
 
   create_table "tasks", force: :cascade do |t|
