a0d3b3e9110194f7aad6a1f58e19b4d97473c298
murahiro <murahiro0919@gmail.com>
2024-11-30 20:09:12 +0900

    PromotionCodeモデルにバリデーションを設定

--- app/models/promotion_code.rb
+++ app/models/promotion_code.rb
@@ -1,2 +1,11 @@
 class PromotionCode < ApplicationRecord
+  validates :code, presence: true,
+                   uniqueness: true,
+                   length: { is: 7 },
+                   format: { with: /\A[a-zA-Z]+\z/ }
+
+  validates :discount_amount, presence: true,
+                              numericality: { onliy_integer: true,
+                                              greather_than_or_equal_to: 100,
+                                              less_than_or_equal_to: 1000 }
 end
