93d0219c9895239cfebf329b0f66601ab2a60136
murahiro <murahiro0919@gmail.com>
2024-12-13 21:41:36 +0900

    タイプミスを修正

--- app/models/promotion_code.rb
+++ app/models/promotion_code.rb
@@ -1,18 +1,18 @@
 class PromotionCode < ApplicationRecord
   has_many :orders, foreign_key: 'used_promotion_code_id'
-  
+
   validates :code, presence: true,
                    uniqueness: true,
                    length: { is: 7 },
                    format: { with: /\A[A-Z0-9]+\z/ }
 
   validates :discount_amount, presence: true,
-                              numericality: { onliy_integer: true,
-                                              greather_than_or_equal_to: 100,
+                              numericality: { only_integer: true,
+                                              greater_than_or_equal_to: 100,
                                               less_than_or_equal_to: 1000 }
 
   def self.generate_code
-    secureRandom,alphanumeric(7).upcase
+    SecureRandom.alphanumeric(7).upcase
   end
 
   def self.generate_discount_amount
